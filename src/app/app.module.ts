ce94c7f44376415823e8a8521f063ae421c204c7
Vinay Nakkina <vinay.1@iitj.ac.in>
2022-12-20 19:15:16 +0530

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { DataComponent } from './data/data.component';
+
+@NgModule({
+  declarations: [AppComponent, DataComponent],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/data/data.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data',
+  templateUrl: './data.component.html',
+  styleUrls: ['./data.component.css'],
+})
+export class DataComponent implements OnInit {
+  // @Input() signupForm: {
+  //   Name: string;
+  //   Id: string;
+  //   Description: string;
+  //   Date: string;
+  // };
+  // @Input() clicked: boolean;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}

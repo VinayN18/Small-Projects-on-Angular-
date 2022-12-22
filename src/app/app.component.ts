12f6d811572b3e1c906e701f3cd6aa277f5103b4
Vinay Nakkina <vinay.1@iitj.ac.in>
2022-12-22 16:13:01 +0530

    Added reactive form approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+/*import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -13,7 +13,6 @@ export class AppComponent {
   displayStyle1 = 'none';
   editRecordId = null;
   formData = [];
-  // open = false;
   user = {
     name: '',
     id: '',
@@ -28,37 +27,12 @@ export class AppComponent {
     date: 'string',
   };
   submitted = false;
-  // listofUsers = [];
   clicked = false;
 
-  // ngOnInit() {
-  //   this.signupForm1 = new FormArray([]);
-  //   this.signupForm = new FormGroup({
-  //     Name: new FormControl(null, Validators.required),
-  //     Id: new FormControl(null, [
-  //       Validators.required,
-  //       Validators.pattern('^[1-9]+[0-9]*$'),
-  //     ]),
-  //     Description: new FormControl(null, Validators.required),
-  //     Date: new FormControl(null, Validators.required),
-  //   });
-  // }
   onClick() {
-    // (<FormArray>this.signupForm1).push(this.signupForm);
     this.displayStyle = 'none';
-    // this.user.name = name;
-    // this.user.id = id;
-    // this.user.description = description;
-    // this.user.date = date;
-    // this.listofUsers.push(this.user);
-    // this.user.name = '';
-    // this.user.id = '';
-    // this.user.description = '';
-    // this.user.date = '';
-    // console.log(this.listofUsers);
   }
   onSubmit() {
-    // console.log(this.signupForm1);
     this.submitted = true;
     if (this.editRecordId) {
       this.formData = this.formData.map((data) =>
@@ -80,11 +54,95 @@ export class AppComponent {
     this.clicked = true;
   }
   onClose() {
-    // this.open = false;
     this.displayStyle = 'none';
   }
   onOpen() {
-    // this.open = true;
+    this.displayStyle = 'block';
+  }
+  onDataClose() {
+    this.displayStyle1 = 'none';
+  }
+  onDelete(user) {
+    this.formData = this.formData.filter((data) => data.id !== user.idee);
+    this.displayStyle1 = 'none';
+  }
+  onRowClick(user) {
+    this.displayStyle1 = 'block';
+    this.details.name = user.Name;
+    this.details.id = user.Id;
+    this.details.description = user.Description;
+    this.details.date = user.Date;
+    this.details.idee = user.id;
+  }
+}
+*/
+
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'Project-Annaya';
+  signupForm: FormGroup;
+  displayStyle = 'none';
+  displayStyle1 = 'none';
+  editRecordId = null;
+  submitted = false;
+  clicked = false;
+  formData = [];
+
+  details = {
+    name: 'string',
+    idee: 'string',
+    id: 'string',
+    description: 'string',
+    date: 'string',
+  };
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      Name: new FormControl(null, Validators.required),
+      Id: new FormControl(null, [
+        Validators.required,
+        Validators.pattern('^[1-9]+[0-9]*$'),
+      ]),
+      Description: new FormControl(null, Validators.required),
+      Date: new FormControl(null, Validators.required),
+    });
+  }
+  onClick() {
+    this.displayStyle = 'none';
+  }
+  onSubmit() {
+    // console.log(this.signupForm.value);
+    this.submitted = true;
+    if (this.editRecordId) {
+      this.formData = this.formData.map((data) =>
+        data.id === this.editRecordId ? this.signupForm.value : data
+      );
+      this.editRecordId = null;
+    } else {
+      const id = Date.now();
+
+      const data = {
+        id,
+        ...this.signupForm.value,
+      };
+      this.formData.push(data);
+    }
+    this.signupForm.reset();
+  }
+  onbuttonclick() {
+    this.clicked = true;
+  }
+  onClose() {
+    this.displayStyle = 'none';
+  }
+  onOpen() {
     this.displayStyle = 'block';
   }
   // onEdit(user) {
@@ -93,26 +151,15 @@ export class AppComponent {
   //   this.editRecordId = idee;
   //   this.Forms.setValue(data);
   // }
-  onGetDetails(user) {
-    // this.displayStyle1 = 'block';
-    // this.details.name = user.Name;
-    // this.details.id = user.Id;
-    // this.details.description = user.Description;
-    // this.details.date = user.Date;
-    // this.details.idee = user.id;
-  }
+
   onDataClose() {
     this.displayStyle1 = 'none';
   }
-  // onCloseOverlay() {
-  //   this.displayStyle1 = 'none';
-  // }
   onDelete(user) {
     this.formData = this.formData.filter((data) => data.id !== user.idee);
     this.displayStyle1 = 'none';
   }
   onRowClick(user) {
-    // console.log(user.Name);
     this.displayStyle1 = 'block';
     this.details.name = user.Name;
     this.details.id = user.Id;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// import { AppComponent } from './app.component';
+// import { DataComponent } from './data/data.component';
+
+// @NgModule({
+//   declarations: [AppComponent, DataComponent],
+//   imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+//   providers: [],
+//   bootstrap: [AppComponent],
+// })
+// export class AppModule {}
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-// import { MatTableModule } from '@angular/material/table';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DataComponent } from './data/data.component';
 
 @NgModule({
   declarations: [AppComponent, DataComponent],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
